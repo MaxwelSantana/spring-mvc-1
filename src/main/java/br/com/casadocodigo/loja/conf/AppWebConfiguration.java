f4a6707af15d151fed275fb061c1ad5eeb998e57
MaxwelSantana <maxwelsant96@icloud.com>
2020-05-23 17:40:54 -0300

    enable caching

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,6 +23,7 @@ import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
 @EnableWebMvc
+@EnableCaching
 @ComponentScan(basePackageClasses = {HomeController.class, ProdutoDAO.class, 
 		FileSaver.class, CarrinhoCompras.class})
 public class AppWebConfiguration {
@@ -66,5 +70,10 @@ public class AppWebConfiguration {
 	@Bean
 	public RestTemplate restTemplate() {
 	    return new RestTemplate();
+	}
+	
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager();
 	}
 }
