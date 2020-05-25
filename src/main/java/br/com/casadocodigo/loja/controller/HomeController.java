f341f539804029408a057b47a830ea21e116fe45
MaxwelSantana <maxwelsant96@icloud.com>
2020-05-25 14:31:04 -0300

    Revert "Merge pull request #6 from MaxwelSantana/develop"
    This reverts commit f98b1290921a668160750c06b1360c1f8fd162f6, reversing
    changes made to bf45c109106cb261484d4456f005d6f36d6f32c0.

--- src/main/java/br/com/casadocodigo/loja/controller/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/HomeController.java
@@ -43,7 +43,7 @@ public class HomeController {
         Usuario usuario = new Usuario(); 
         usuario.setNome("Admin");
         usuario.setEmail("admin@casadocodigo.com.br");
-        usuario.setSenha("$04$qP517gz1KNVEJUTCkUQCY.JzEoXzHFjLAhPQjrg5iP6Z/UmWjvUhq");
+        usuario.setSenha("$2a$10$lt7pS7Kxxe5JfP.vjLNSyOXP11eHgh7RoPxo5fvvbMCZkCUss2DGu");
         usuario.setRoles(Arrays.asList(new Role("ROLE_ADMIN")));
         usuarioDao.gravar(usuario);
 
